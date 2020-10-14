c4836c8c289b69e7bfc5c2e8c58f2fff8fe2aeb7
Nawaf Alharbi <nawaf.manage@gmail.com>
2020-10-14 19:18:44 +0300

    modifications to Makefile

--- microservices_practice/DB_Setup/init.sh
+++
@@ -1,7 +0,0 @@
-docker build -t nawaf-sql .
-docker run --name sql-container -d -p 8080:30 nawaf-sql
-echo "Waiting For DB..."
-docker exec sql-container mysqladmin --silent --wait=30 -uNawafUser -pNawafPass ping || exit 1 
-echo "Inserting data..."
-sleep 70
-docker exec -i sql-container mysql -uNawafUser -pNawafPass NawafDB < setup.sql
